020404f6d79ef41bd542e85984e901ce6f575980
vidi <vidialpha@gmail.com>
2026-01-28 12:15:18 +0700

    perubahan

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/',  // Pastikan ini '/' untuk root domain
+  base: '/',
   build: {
     outDir: 'dist',
     rollupOptions: {
